e3148d23e71cfbc7ea2abac4475b90513f0d6493
Ben Metzger <benjamin_metzger@gensler.com>
2013-02-18 16:41:52 -0600

    changes for using the html5 boilerplate

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,11 @@
 require.config({
+  baseUrl: '../',
   paths: {
     'jquery': 'vendor/jquery/jquery',
     'underscore': 'vendor/underscore-amd/underscore',
     'backbone': 'vendor/backbone-amd/backbone',
-    'handlebars': 'vendor/handlebars/handlebars'
+    'handlebars': 'vendor/handlebars/handlebars',
+    'modernizr': 'vendor/modernizr/index'
   }
 });
 
