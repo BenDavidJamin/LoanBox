438602b222b509eaa2eebe0d2680a736f48dcceb
Ben Metzger <benjamin_metzger@gensler.com>
2013-04-08 10:02:11 -0500

    adding support for testem setup with requirejs

---
+++ app/tests/ATest.js
@@ -0,0 +1,9 @@
+define([""], function (){
+  describe("A Test", function(){
+    it("should have a true or false return value", function(){
+      var value = true;
+      expect(value).toBe(true);
+    });
+  });
+});
+
