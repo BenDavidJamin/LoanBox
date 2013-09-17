5d88426a4c3029969d2f455eb6584fd75d7338ea
Benjamin Metzger <benjamin_metzger@gensler.com>
2013-09-17 09:17:52 -0500

    adding init script for setting up the git hooks and installing npm bower

---
+++ init.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+npm install
+
+bower install
+
+# clone in hooks and remove all but the wanted grunt related 
+# hooks
+
+#clean out the hook examples 
+rm .git/hooks/*
+
+git clone "git@gitlab.genslerwi.com:benjamin_metzger/gis-git-hooks.git" .git/hooks
+
+cd .git/hooks
+
+git checkout ssp
+
+git filter-branch --prune-empty --subdirectory-filter grunt -- --all
+
+git filter-branch -f --prune-empty --index-filter 'git rm --cached --ignore-unmatch $(git ls-files | grep -v "pre-commit.sh")'
